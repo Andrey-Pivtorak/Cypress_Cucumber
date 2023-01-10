662239ef1df00117bc2eff7f0157bf0dd5a03325
Andrey <2593147@gmail.com>
2023-01-10 20:18:47 +0100

    update file

--- cypress/e2e/step_definitions/developerTabs.js
+++ cypress/e2e/step_definitions/developerTabs.js
@@ -9,12 +9,12 @@ When('Click the "API v1" tab', () => {
   developersPage.clickElement(developersPage.api1Button);
 });
 Then('The Telnyx "API V1 Documentation" text is displayed', () => {
-  cy.wait(2000).get(developersPage.api1Title).should('contain', 'Telnyx API V1 Documentation');
+  cy.wait(4000).get(developersPage.api1Title).should('contain', 'Telnyx API V1 Documentation');
 });
 
 When('Click the "API v2" tab', () => {
   developersPage.clickElement(developersPage.api2Button);
 });
 Then('The "Telnyx API Documentation" text is displayed', () => {
-  cy.wait(2000).get(developersPage.api2Title).should('have.text', 'Telnyx API Documentation');
+  cy.wait(4000).get(developersPage.api2Title).should('have.text', 'Telnyx API Documentation');
 });
